38c9ad4846d3148876a26e94c8ca4dbfc020c42d
Kacper Kowalski <kkowalski0477@gmail.com>
2025-03-19 14:13:51 +0100

    Update
    Learnt more about the DOM.

--- .vscode/Javascript/DOM/index.js
+++ .vscode/Javascript/DOM/index.js
@@ -3,3 +3,12 @@ document.query
 var thirdLiElement = document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild;
 
 thirdLiElement.innerHTML = "ESSA";
+
+var innerGoogleAnchor = document.querySelector(".list a");
+
+innerGoogleAnchor.style.color = "red";
+
+var yellowButton = document.querySelector("button");
+yellowButton.style.backgroundColor = "yellow";
+
+var hello = document.querySelector("h1").classList.add("huge");
