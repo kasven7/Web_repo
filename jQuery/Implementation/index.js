4f06fa9253b453cfa15553a014c3a778474a3853
Kacper Kowalski <kkowalski0477@gmail.com>
2025-03-30 19:16:49 +0200

    jQuery stuff

---
+++ jQuery/Implementation/index.js
@@ -0,0 +1,4 @@
+// $ or jQuery it is the same thing
+/* $(document).ready(function(){}); - once the document is ready then it will execute all that is inside of the brackets*/
+$("h1").css("color", "blue");
+$("h1").addClass("big-title", "margin-80");
