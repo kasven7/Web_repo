6758dfbb5ce3df26ac791dc543b33d6ce2c5979e
Smitheyy <kkowalski0477@gmail.com>
2025-03-31 12:12:19 +0200

    Some more jQuery stuff

--- jQuery/Implementation/index.js
+++ jQuery/Implementation/index.js
@@ -2,3 +2,7 @@
 /* $(document).ready(function(){}); - once the document is ready then it will execute all that is inside of the brackets*/
 $("h1").css("color", "blue");
 $("h1").addClass("big-title", "margin-80");
+
+$(document).keydown(function(event){
+    $("h1").text(event.key);
+});
