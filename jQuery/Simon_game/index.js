8d225b83e1f94afebc257ba0c8c32b7afe80b236
Kacper Kowalski <kkowalski0477@gmail.com>
2025-04-03 00:40:06 +0200

    Update
    This still needs to be fixed but it should be done already, don't know why it does not work...

--- jQuery/Simon_game/index.js
+++ jQuery/Simon_game/index.js
@@ -1,19 +1,82 @@
 var gamePattern = [];
+var userClickedPattern = [];
 var buttonColors = ["red", "green", "blue", "yellow"];
+var started = false;
+var level = 0;
+
+$(document).on("keydown", function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
 $(".btn").on("click", function () {
   var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userChosenColor.length - 1);
 });
 
 function nextSequence() {
   var randomNum = Math.floor(Math.random() * 4);
   var randomColor = buttonColors[randomNum];
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
   gamePattern.push(randomColor);
   $("#" + randomColor)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
 
-  var audio = new Audio("./sounds/" + randomColor + ".mp3");
+  playSound(randomColor);
+}
+
+function playSound(name) {
+  var audio = new Audio("./sounds/" + name + ".mp3");
   audio.play();
+}
+
+function animatePress(currentColor) {
+  var button = $("#" + currentColor);
+  button.addClass("pressed");
+
+  setTimeoutOut(function () {
+    button.removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("Success!");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
