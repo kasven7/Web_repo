0fa4b794ea1c0448c853f2844c0505f3ee6c2a93
Smitheyy <kkowalski0477@gmail.com>
2025-06-11 14:08:02 +0200

    Travel_Tracker update
    Added a post route that allows the user to add a country onto the visited list, once added it will be highlighted on the map. Added a delete button onto the form I still have to figure out how to discern it from the add button and how to chandle both of them on the same post route - this is work-in-progress.

--- Backend/Travel_Tracker/index.js
+++ Backend/Travel_Tracker/index.js
@@ -18,15 +18,53 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
+async function checkVisited(){
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
+
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisited();
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
+});
+
+app.post("/modify", async (req, res) => {
+  try{
+  const userInput = req.body["country"];
+
+  const result = await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) like '%' || ($1) || '%'", [userInput.toLowerCase()]);
+    
+  const countryCode = result.rows[0].country_code;
+
+  try{
+    await db.query(
+    "INSERT INTO visited_countries (country_code) VALUES ($1)",
+    [countryCode]
+  );
+  res.redirect("/");
+  } catch(error){
+    console.log(error);
+    const countries = await checkVisited();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country has already been added. Try again!",
+    });
+  }
+  } catch(error){
+    console.log(error);
+    const countries = await checkVisited();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist. Try again!",
+    });
+  }
 });
 
 app.listen(port, () => {
